aba1eb25777f951dd42b2d516b298fa72c6f6858
Paulo Gomes <61259138+pgexec@users.noreply.github.com>
2024-08-13 09:01:06 -0300

    Add files via upload

---
+++ projetoDeIngles/script/script.js
@@ -0,0 +1,226 @@
+const dicionario = {
+    A: { 
+        ingles: ["Abeyance"],
+        traduzido: ["Suspensão"],
+        frase:["The decision was held in abeyance until all evidence was considered."],
+        imagem: ["img/Abeyance.png"],
+        audio: ["audio/A.mp3"]
+    },
+    B: { 
+        ingles: ["Belligerent"],
+        traduzido: ["Beligerante"],
+        frase:["The belligerent tone of the negotiations made it difficult to reach an agreement."],
+        imagem: ["img/b.png"],
+        audio: ["audio/b.mp3"]
+    },
+    C: { 
+        ingles: ["Cacophony"],
+        traduzido: ["Cacofonia"],
+        frase:["The cacophony of the city traffic can be overwhelming."],
+        imagem: ["img/Cacophony.png"],
+        audio: ["audio/C.mp3"]
+    },
+    D: { 
+        ingles: ["Demagogue"],
+        traduzido: ["Demagogo"],
+        frase:["The politician was a demagogue who preyed on people's fears and prejudices."],
+        imagem: ["img/Demagogue.png"],
+        audio: ["audio/D.mp3"]
+    },
+    E: { 
+        ingles: ["Ebullience"],
+        traduzido: ["Efervescência"],
+        frase:["His ebullience was infectious, lighting up the room with joy."],
+        imagem: ["img/Ebullience.png"],
+        audio: ["audio/E.mp3"]
+    },
+    F: { 
+        ingles: ["Felicitous"],
+        traduzido: ["Feliz, bem escolhido"],
+        frase:["Her choice of words was felicitous, capturing the mood perfectly."],
+        imagem: ["img/Felicitous.jpg"],
+        audio: ["audio/F.mp3"]
+    },
+    G: { 
+        ingles: ["Garrulous"],
+        traduzido: ["tagarela"],
+        frase:["The garrulous host kept us entertained all evening."],
+        imagem: ["img/talk-o-meter-31.png"],
+        audio: ["audio/G.mp3"]
+    },
+    H: { 
+        ingles: ["Halcyon"],
+        traduzido: ["pacífico, calmo"],
+        frase:["They longed for the halcyon days of their youth."],
+        imagem: ["img/Halcyon.png"],
+        audio: ["audio/H.mp3"]
+    },
+    I: { 
+        ingles: ["Ineffable"],
+        traduzido: ["Indescritível"],
+        frase:["The beauty of the sunset was ineffable."],
+        imagem: ["img/Ineffable.png"],
+        audio: ["audio/i.mp3"]
+    },
+    J: { 
+        ingles: ["Jejune"],
+        traduzido: ["Insípido, sem interesse"],
+        frase:["The lecture was jejune and lacked depth."],
+        imagem: ["img/Jejune.png"],
+        audio: ["audio/J.mp3"]
+    },
+    K: { 
+        ingles: ["Kaleidoscope"],
+        traduzido: ["Caleidoscópio"],
+        frase:["Her dress was a kaleidoscope of vibrant colors."],
+        imagem: ["img/Kaleidoscope.png"],
+        audio: ["audio/K.mp3"]
+    },
+    L: { 
+        ingles: ["Lugubrious"],
+        traduzido: ["Lúgubre, triste"],
+        frase:["The lugubrious music set a somber tone for the evening."],
+        imagem: ["img/Lugubrious.png"],
+        audio: ["audio/L.mp3"]
+    },
+    M: { 
+        ingles: ["Munificent"],
+        traduzido: ["Muito generoso"],
+        frase:["The munificent donation to the charity was greatly appreciated."],
+        imagem: ["img/Munificent.png"],
+        audio: ["audio/M.mp3"]
+    },
+    N: { 
+        ingles: ["Nebulous"],
+        traduzido: ["Nebuloso, vago"],
+        frase:["Her plans for the future were still nebulous and undefined."],
+        imagem: ["img/Nebulous.png"],
+        audio: ["audio/N.mp3"]
+    },
+    O: { 
+        ingles: ["Obfuscate"],
+        traduzido: [" Ofuscar, obscurecer"],
+        frase:["He tried to obfuscate the issue with irrelevant facts"],
+        imagem: ["img/Obfuscate.png"],
+        audio: ["audio/O.mp3"]
+    },
+    P: { 
+        ingles: ["Pernicious"],
+        traduzido: ["Perigoso, nocivo"],
+        frase:["Smoking is known to have pernicious effects on lung health."],
+        imagem: ["img/pernicious.png"],
+        audio: ["audio/P.mp3"]
+    },
+    Q: { 
+        ingles: ["Quintessential"],
+        traduzido: ["Quintessencial, a mais perfeita manifestação de algo"],
+        frase:["She is the quintessential example of a successful entrepreneur."],
+        imagem: ["img/einsten.png"],
+        audio: ["audio/q.mp3"]
+    },
+    R: { 
+        ingles: ["Reticent"],
+        traduzido: [" Reticente, reservado"],
+        frase:["He was reticent about his plans, preferring to keep them private."],
+        imagem: ["img/Reticent.png"],
+        audio: ["audio/r.mp3"]
+    },
+    S: { 
+        ingles: ["Surreptitious"],
+        traduzido: ["Sorrateiro"],
+        frase:["He gave her a surreptitious glance across the room."],
+        imagem: ["img/surreptitious.png"],
+        audio: ["audio/S.mp3"]
+    },
+    T: { 
+        ingles: ["Taciturn"],
+        traduzido: ["Taciturno"],
+        frase:["The taciturn man spoke very little, often responding with just a nod."],
+        imagem: ["img/Taciturn.png"],
+        audio: ["audio/T.mp3"]
+    },
+    U: { 
+        ingles: ["Ubiquitous"],
+        traduzido: ["Ubíquo, onipresente"],
+        frase:["Smartphones are ubiquitous nowadays, seen in every hand."],
+        imagem: ["img/Ubiquitous.png"],
+        audio: ["audio/u.mp3"]
+    },
+    V: { 
+        ingles: ["Verisimilitude"],
+        traduzido: ["Verossimilhança"],
+        frase:["The novel's attention to detail lent it an air of verisimilitude."],
+        imagem: ["img/Verossimilhança.png"],
+        audio: ["audio/V.mp3"]
+    },
+    W: { 
+        ingles: ["Winsome"],
+        traduzido: ["Atraente, cativante"],
+        frase:["Her winsome smile won the hearts of everyone in the room."],
+        imagem: ["img/winsome.png"],
+        audio: ["audio/w.mp3"]
+    },
+    X: { 
+        ingles: ["Xenophile"],
+        traduzido: ["Xenófilo"],
+        frase:["As a xenophile, she always enjoyed exploring diverse cultures and learning new languages."],
+        imagem: ["img/zelo.png"],
+        audio: ["audio/X.mp3"]
+    },
+    Y: { 
+        ingles: ["Yield "],
+        traduzido: ["Rendimento"],
+        frase:["nvestors are interested in the yield of this stock."],
+        imagem: ["img/yield.png"],
+        audio: ["audio/Y.mp3"]
+    },
+    Z: { 
+        ingles: [" Zeal "],
+        traduzido: ["Zelo "],
+        frase:["She tackled her work with great zeal and dedication."],
+        imagem: ["img/zelo.png"],
+        audio: ["audio/z.mp3"]
+    },
+    
+};
+
+document.querySelectorAll('.botao').forEach(botao => {
+    botao.addEventListener('click', function(){
+        const letra = this.getAttribute('letra');
+        const dados = dicionario[letra];
+
+        const palavraIngles = document.querySelector('.palavraIngles');
+        const palavraTraduzida = document.querySelector('.palavraTraduzida');
+        const frase = document.querySelector('.Frase');
+
+        let image = document.createElement('img');
+        let source = document.createElement('source');
+        let imgContainer = document.querySelector('.img-container');
+        let audioContainer = document.querySelector('.audio-container');
+        let audio = document.createElement('audio');
+
+        if (dados) {
+            palavraIngles.textContent = dados.ingles.join(", ");
+            palavraTraduzida.textContent = dados.traduzido.join(", ");
+            frase.textContent = dados.frase;
+            image.src = dados.imagem;
+            imgContainer.innerHTML = '';
+            imgContainer.appendChild(image);
+            
+            source.setAttribute('src', dados.audio[0]);
+            source.setAttribute('type', 'audio/mpeg');
+            audio.setAttribute('controls', ''); // Adicionar controles de reprodução
+            audio.appendChild(source);
+            audioContainer.innerHTML = '';
+            audioContainer.appendChild(audio);
+            
+        
+
+
+
+        } else {
+            palavraIngles.textContent = "N/A";
+            palavraTraduzida.textContent = "N/A";
+        }
+    });
+});
